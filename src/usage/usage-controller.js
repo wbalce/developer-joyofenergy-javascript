12c1665be85dff7494caf7c91d2492df0ae9a64c
Wilkinson Balce <wilkinson.balce@gmail.com>
2021-06-11 07:51:41 +1000

    Use unix time instead of epoch as input param

--- src/usage/usage-controller.js
+++ src/usage/usage-controller.js
@@ -7,8 +7,10 @@ const getUsageCostForPreviousWeek = (getReadings, req) => {
 
     if (!pricePlan) throw new Error("The given meter does not have a price plan.");
 
-    return calculateUsageCostForPreviousWeek(
-	getReadings(meterId), pricePlan.rate, (new Date()).getTime());
+    const usageCostForPreviousWeek = calculateUsageCostForPreviousWeek(
+	getReadings(meterId), pricePlan.rate, ((new Date()).getTime() / 1000));
+
+    return { usageCostForPreviousWeek }
 };
 
 module.exports = {
