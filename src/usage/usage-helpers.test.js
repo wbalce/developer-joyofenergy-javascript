eeeb484382d9c9882770b05b73cae9883a53d6b7
Wilkinson Balce <wilkinson.balce@gmail.com>
2021-06-10 22:07:57 +1000

    Use more descriptive unit test names

--- src/usage/usage-helpers.test.js
+++ src/usage/usage-helpers.test.js
@@ -14,19 +14,19 @@ const MONDAY_INTEGER = 1;
 const SUNDAY_INTEGER = 0;
 
 describe("usage-helpers", () => {
-  it("should get last given day", () => {
+  it("should get unix time of the previous instance of a day from a reference unix time", () => {
     const result = getLastGivenDayUnixTime(MOCK_UNIX_TIME_FRIDAY, SUNDAY_INTEGER);
 
     expect(result).toBe(MOCK_UNIX_TIMES_PREVIOUS_SUNDAY_MIDNIGHT);
   });
 
-  it("should get given day from last week", () => {
+  it("should get given day from previous week of a reference unix time", () => {
     const result = getDayFromLastWeekUnixTime(MOCK_UNIX_TIME_FRIDAY, MONDAY_INTEGER);
 
     expect(result).toBe(MOCK_UNIX_TIMES_PREVIOUS_WEEK_MONDAY_MIDNIGHT);
   });
 
-  it("should get start and end of last week", () => {
+  it("should get start and end of previous week of a reference unix time", () => {
     const result = getStartAndEndOfLastWeekUnixTimes(MOCK_UNIX_TIME_FRIDAY);
 
     expect(result).toMatchObject({
@@ -35,19 +35,19 @@ describe("usage-helpers", () => {
     });
   });
 
-  it("should return true if given unix time is from previous week", () => {
+  it("should return true if given unix time is from previous week of a reference unix time", () => {
     const result = isWithinPreviousWeekToGivenReferenceTime(MOCK_UNIX_TIME_FRIDAY, MOCK_UNIX_TIMES_PREVIOUS_SUNDAY_MIDNIGHT);
 
     expect(result).toBe(true);
   });
 
-  it("should return false if given unix time not from previous week", () => {
+  it("should return false if given unix time is not from previous week of a reference unix time", () => {
     const result = isWithinPreviousWeekToGivenReferenceTime(MOCK_UNIX_TIME_FRIDAY, MOCK_UNIX_TIME_FRIDAY);
 
     expect(result).toBe(false);
   });
 
-  it("should return false if given unix time from two weeks ago", () => {
+  it("should return false if given unix time is from two weeks prior to a reference unix time", () => {
     const result = isWithinPreviousWeekToGivenReferenceTime(MOCK_UNIX_TIME_FRIDAY, MOCK_UNIX_TIMES_TWO_PREVIOUS_WEEKS_SUNDAY_MIDNIGHT);
 
     expect(result).toBe(false);
