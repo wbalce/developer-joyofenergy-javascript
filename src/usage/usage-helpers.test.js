205b68e814cf3596c7d1d07a225678395b8abe0b
Wilkinson Balce <wilkinson.balce@gmail.com>
2021-06-10 19:41:24 +1000

    refactor tests by extracting mock input and values to object

--- src/usage/usage-helpers.test.js
+++ src/usage/usage-helpers.test.js
@@ -4,36 +4,31 @@ const {
   getStartAndEndOfLastWeekUnixTimes
 } = require('./usage-helpers');
 
+const mockUnixTimes = {
+  reference: 1607686125, // Friday, 11 December 2020 11:28:45 GMT+00:00
+  previousSunday: 1607212800, // Sunday, 06 December 2020 00:00:00 GMT+00:00
+  previousWeekMonday: 1606694400, // Monday, 30 November 2020 00:00:00 GMT+00:00
+};
+
 describe("usage-helpers", () => {
   it("should get last given day", () => {
-    const referenceUnixTime = 1607686125; // Friday, 11 December 2020 11:28:45 GMT+00:00
-    const lastSundayUnixTime = 1607212800; // Sunday, 06 December 2020 00:00:00 GMT+00:00
+    const result = getLastGivenDayUnixTime(mockUnixTimes.reference, 0);
 
-    const result = getLastGivenDayUnixTime(referenceUnixTime, 0);
-
-    expect(result).toBe(lastSundayUnixTime);
+    expect(result).toBe(mockUnixTimes.previousSunday);
   });
 
   it("should get given day from last week", () => {
-    const referenceUnixTime = 1607686125; // Friday, 11 December 2020 11:28:45 GMT+00:00
-    const lastMondayUnixTime = 1606694400; // Monday, 30 November 2020 00:00:00 GMT+00:00
-    
-    const result = getDayFromLastWeekUnixTime(referenceUnixTime, 1);
+    const result = getDayFromLastWeekUnixTime(mockUnixTimes.reference, 1);
 
-    expect(result).toBe(lastMondayUnixTime);
+    expect(result).toBe(mockUnixTimes.previousWeekMonday);
   });
 
   it("should get start and end of last week", () => {
-    const referenceUnixTime = 1607686125; // Friday, 11 December 2020 11:28:45 GMT+00:00
-
-    const lastMondayUnixTime = 1606694400 // Monday, 30 November 2020 00:00:00 GMT+00:00
-    const lastSundayUnixTime = 1607212800; // Sunday, 06 December 2020 00:00:00 GMT+00:00
-
-    const result = getStartAndEndOfLastWeekUnixTimes(referenceUnixTime);
+    const result = getStartAndEndOfLastWeekUnixTimes(mockUnixTimes.reference);
 
     expect(result).toMatchObject({
-      startDayUnixTime: lastMondayUnixTime,
-      endDayUnixTime: lastSundayUnixTime
+      startDayUnixTime: mockUnixTimes.previousWeekMonday,
+      endDayUnixTime: mockUnixTimes.previousSunday
     });
   });
 });
