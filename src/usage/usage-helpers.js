db7d7cd7a32fd58370812823d40f4908b3803876
Wilkinson Balce <wilkinson.balce@gmail.com>
2021-06-11 01:23:24 +1000

    Fix boundary value issues by picking the correct sunday unix time

--- src/usage/usage-helpers.js
+++ src/usage/usage-helpers.js
@@ -1,19 +1,30 @@
-const getLastGivenDayUnixTime = (currentUnixTime, requiredDayInt) => {
+const getUnixTimeByNumberOfDaysInThePast = (currentUnixTime, numberOfDaysInThePast) => {
     const secondsIn24Hours = 60 * 60 * 24;
+    const lastMidnightUnixTime = new Date(currentUnixTime * 1000).setUTCHours(0, 0, 0, 0) / 1000;
 
-    const currentDate = new Date(currentUnixTime * 1000);
-    const currentDay = currentDate.getDay();
+    return lastMidnightUnixTime - numberOfDaysInThePast * secondsIn24Hours;
+};
 
-    const lastMidnightUnixTime = currentDate.setUTCHours(0, 0, 0, 0) / 1000;
-    const difference = currentDay - requiredDayInt;
+const getLastGivenDayUnixTime = (currentUnixTime, requiredDayInteger) => {
+    const currentDayInteger = (new Date(currentUnixTime * 1000)).getUTCDay();
+
+    const difference = currentDayInteger - requiredDayInteger;
     const numberOfDaysBetween = difference < 0
         ? (7 + difference) % 7
         : difference % 7;
- 
-    return lastMidnightUnixTime - numberOfDaysBetween * secondsIn24Hours;
+
+    return getUnixTimeByNumberOfDaysInThePast(currentUnixTime, numberOfDaysBetween);
+};
+
+const isSunday = (unixTime) => {
+    return (new Date(unixTime * 1000)).getUTCDay() === 0;
 };
 
 const getSundayLastWeek = (currentUnixTime) => {
+    if (isSunday(currentUnixTime)) {
+        return getUnixTimeByNumberOfDaysInThePast(currentUnixTime, 7);
+    }
+
     return getLastGivenDayUnixTime(currentUnixTime, 0);
 };
 
--- src/usage/usage-helpers.test.js
+++ src/usage/usage-helpers.test.js
@@ -5,19 +5,22 @@ const {
   isWithinPreviousWeekToGivenReferenceTime
 } = require('./usage-helpers');
 
+const MOCK_UNIX_TIME_SUNDAY = 1607817600;
 const MOCK_UNIX_TIME_FRIDAY = 1607686125; // Friday, 11 December 2020 11:28:45 GMT+00:00
-const MOCK_UNIX_TIMES_PREVIOUS_SUNDAY_MIDNIGHT = 1607212800;
+const MOCK_UNIX_TIME_MONDAY = 1607299200;
+const MOCK_UNIX_TIMES_PREVIOUS_WEEK_FRIDAY_MIDNIGHT = 1607040000; 
+const MOCK_UNIX_TIMES_PREVIOUS_WEEK_SUNDAY_MIDNIGHT = 1607212800;
 const MOCK_UNIX_TIMES_PREVIOUS_WEEK_MONDAY_MIDNIGHT = 1606694400;
 const MOCK_UNIX_TIMES_TWO_PREVIOUS_WEEKS_SUNDAY_MIDNIGHT = 1606608000;
 
-const MONDAY_INTEGER = 1;
 const SUNDAY_INTEGER = 0;
+const MONDAY_INTEGER = 1;
 
 describe("usage-helpers", () => {
   it("should get unix time of the previous instance of a day from a reference unix time", () => {
     const result = getLastGivenDayUnixTime(MOCK_UNIX_TIME_FRIDAY, SUNDAY_INTEGER);
 
-    expect(result).toBe(MOCK_UNIX_TIMES_PREVIOUS_SUNDAY_MIDNIGHT);
+    expect(result).toBe(MOCK_UNIX_TIMES_PREVIOUS_WEEK_SUNDAY_MIDNIGHT);
   });
 
   it("should get given day from previous week of a reference unix time", () => {
@@ -28,15 +31,45 @@ describe("usage-helpers", () => {
 
   it("should get start and end of previous week of a reference unix time", () => {
     const result = getStartAndEndOfLastWeekUnixTimes(MOCK_UNIX_TIME_FRIDAY);
-
+ 
     expect(result).toMatchObject({
       startDayUnixTime: MOCK_UNIX_TIMES_PREVIOUS_WEEK_MONDAY_MIDNIGHT,
-      endDayUnixTime: MOCK_UNIX_TIMES_PREVIOUS_SUNDAY_MIDNIGHT
+      endDayUnixTime: MOCK_UNIX_TIMES_PREVIOUS_WEEK_SUNDAY_MIDNIGHT
     });
   });
 
-  it("should return true if given unix time is from previous week of a reference unix time", () => {
-    const result = isWithinPreviousWeekToGivenReferenceTime(MOCK_UNIX_TIME_FRIDAY, MOCK_UNIX_TIMES_PREVIOUS_SUNDAY_MIDNIGHT);
+  it("should get start and end of previous week even if reference unix time is a start-of-week boundary", () => {
+    const result = getStartAndEndOfLastWeekUnixTimes(MOCK_UNIX_TIME_MONDAY);
+
+    expect(result).toMatchObject({
+      startDayUnixTime: MOCK_UNIX_TIMES_PREVIOUS_WEEK_MONDAY_MIDNIGHT,
+      endDayUnixTime: MOCK_UNIX_TIMES_PREVIOUS_WEEK_SUNDAY_MIDNIGHT
+    });
+  });
+
+  it("should get start and end of previous week even if reference unix time is an end-of-week boundary", () => {
+    const result = getStartAndEndOfLastWeekUnixTimes(MOCK_UNIX_TIME_SUNDAY);
+ 
+    expect(result).toMatchObject({
+      startDayUnixTime: MOCK_UNIX_TIMES_PREVIOUS_WEEK_MONDAY_MIDNIGHT,
+      endDayUnixTime: MOCK_UNIX_TIMES_PREVIOUS_WEEK_SUNDAY_MIDNIGHT
+    });
+  });
+
+  it("should return true if given unix time is starting boundary value of previous week of a reference unix time", () => {
+    const result = isWithinPreviousWeekToGivenReferenceTime(MOCK_UNIX_TIME_FRIDAY, MOCK_UNIX_TIMES_PREVIOUS_WEEK_MONDAY_MIDNIGHT);
+
+    expect(result).toBe(true);
+  });
+
+  it("should return true if given unix time is ending boundary value of previous week of a reference unix time", () => {
+    const result = isWithinPreviousWeekToGivenReferenceTime(MOCK_UNIX_TIME_FRIDAY, MOCK_UNIX_TIMES_PREVIOUS_WEEK_SUNDAY_MIDNIGHT);
+
+    expect(result).toBe(true);
+  });
+
+  it("should return true if given unix time is in previous week of a reference unix time", () => {
+    const result = isWithinPreviousWeekToGivenReferenceTime(MOCK_UNIX_TIME_FRIDAY, MOCK_UNIX_TIMES_PREVIOUS_WEEK_FRIDAY_MIDNIGHT);
 
     expect(result).toBe(true);
   });
