5c128aeabdea250fc8611d9e2a5d9a738f6ba42a
Wilkinson Balce <wilkinson.balce@gmail.com>
2021-06-10 19:34:26 +1000

    Add function to get instance of given day from previous week defined as monday to sunday

--- src/usage/usage-helpers.js
+++ src/usage/usage-helpers.js
@@ -13,6 +13,18 @@ const getLastGivenDayUnixTime = (currentUnixTime, requiredDayInt) => {
     return lastMidnightUnixTime - numberOfDaysBetween * secondsIn24Hours;
 };
 
+
+const getSundayLastWeek = (currentUnixTime) => {
+    return getLastGivenDayUnixTime(currentUnixTime, 0);
+};
+
+const getDayFromLastWeekUnixTime = (currentUnixTime, requiredDayInt) => {
+    const lastSunday = getSundayLastWeek(currentUnixTime);
+
+    return getLastGivenDayUnixTime(lastSunday, requiredDayInt);
+};
+
 module.exports = {
-  getLastGivenDayUnixTime
+  getLastGivenDayUnixTime,
+  getDayFromLastWeekUnixTime
 };
--- src/usage/usage-helpers.test.js
+++ src/usage/usage-helpers.test.js
@@ -1,5 +1,6 @@
 const {
   getLastGivenDayUnixTime,
+  getDayFromLastWeekUnixTime
 } = require('./usage-helpers');
 
 describe("usage-helpers", () => {
@@ -10,5 +11,14 @@ describe("usage-helpers", () => {
     const result = getLastGivenDayUnixTime(referenceUnixTime, 0);
 
     expect(result).toBe(lastSundayUnixTime);
-  }); 
+  });
+
+  it("should get given day from last week", () => {
+    const referenceUnixTime = 1607686125; // Friday, 11 December 2020 11:28:45 GMT+00:00
+    const lastMondayUnixTime = 1606694400; // Monday, 30 November 2020 00:00:00 GMT+00:00
+    
+    const result = getDayFromLastWeekUnixTime(referenceUnixTime, 1);
+
+    expect(result).toBe(lastMondayUnixTime);
+  });
 });
